e34f8362cfaa1112bd072291ea0fb6fec4056d97
ShihanZENG <160913593+ShihanZENG@users.noreply.github.com>
2024-05-13 09:43:28 +0100

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -3,48 +3,39 @@ from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+# from langchain_openai import OpenAIEmbeddings
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain, ConversationChain, RetrievalQA
+from langchain_community.llms import HuggingFaceHub
+from langchain.llms import OpenAI
 from langchain.vectorstores import Qdrant
 import qdrant_client
 from htmlTemplates import css, bot_template, user_template
-from langchain.prompts import PromptTemplate
-
-# Define templates for generating responses to questions
-template_v1 = PromptTemplate(
-    template="""Answer the questions based on the following document context. 
-    Please quote as much of the document as possible and make sure your answer is concise. 
-    Question: {question}""",
-    input_variables=["context", "question"]
-)
-
 def get_pdf_text(pdf_docs):
-    # Extract text from multiple PDF files
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)  # 更新为使用PdfReader
         for page in pdf_reader.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + ' '
-    return text.strip()
-
+            text += page.extract_text() if page.extract_text() else ''
+    return text
 def get_text_chunks(raw_text):
-    # Split long text into sizes suitable for processing
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    return text_splitter.split_text(raw_text)
+    chunks = text_splitter.split_text(raw_text)  # 假设正确的方法是 split_text 而不是 splitter.split_text
+    return chunks
 
 def get_vectorstore(text_chunks, use_qdrant=True):
-    # Create or update a vector store based on a block of text
     embeddings = OpenAIEmbeddings()
     if use_qdrant:
+        # 如果使用Qdrant，确保已经设置了环境变量
         client = qdrant_client.QdrantClient(
             os.getenv("QDRANT_HOST"),
             api_key=os.getenv("QDRANT_API_KEY")
@@ -54,53 +45,73 @@ def get_vectorstore(text_chunks, use_qdrant=True):
             collection_name=os.getenv("QDRANT_COLLECTION_NAME"),
             embeddings=embeddings,
         )
+    else:
+        # 如果不使用Qdrant，则使用FAISS
+        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
     return vectorstore
+# def get_vectorstore(text_chunks):
+#     embeddings = OpenAIEmbeddings()
+#     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     return vectorstore
+#
+# # model = INSTRUCTOR('hkunlp/instructor-large')
 
 def get_conversation_chain(vectorstore):
-    # Creating a session chain, combining LLM and a retriever
     llm = ChatOpenAI()
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
     return conversation_chain
-
-def handle_userinput(user_question, pdf_text):
-    # Processes user input and displays responses via Streamlit
-    full_prompt = template_v1.format(context=pdf_text, question=user_question)  # Generate complete prompts using document content and user questions
-    response = st.session_state.conversation({'question': full_prompt})
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
-    # 输出聊天历史
-    for i, message in enumerate(st.session_state.chat_history):
-        template = user_template if i % 2 == 0 else bot_template
-        st.write(template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-
+    for i,message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}",message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+    # st.write(response)
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.set_page_config(
+        page_title="Chat with multiple PDFs",
+        page_icon=":books")
     st.write(css, unsafe_allow_html=True)
-    if "conversation" not in st.session_state:
+
+    if"conversation" not in st.session_state:
         st.session_state.conversation = None
-    st.sidebar.header("Upload your PDFs")
-    pdf_docs = st.sidebar.file_uploader("Upload your PDFs here", accept_multiple_files=True)
-    if pdf_docs:
-        pdf_text = get_pdf_text(pdf_docs)  # Get all the text content of the PDF, but do not display the
-        st.header("Chat with multiple PDFs :book:")
-        user_question = st.text_input("Ask a question about your documents:", key="user_question")
-        if user_question:
-            handle_userinput(user_question, pdf_text)
-        if st.sidebar.button("Process", key="process_button"):
-            with st.spinner("Processing..."):
-                text_chunks = get_text_chunks(pdf_text)
-                st.sidebar.write(text_chunks)
-                vectorstore = get_vectorstore(text_chunks)
-                st.success("You can ask questions now.")
-                st.session_state.conversation = get_conversation_chain(vectorstore)
-    else:
-        st.warning("Please upload at least one PDF file.")
+    if"chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :book:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your document")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            if pdf_docs:  # 检查是否有文件被上传
+                with st.spinner("Processing"):
+                    # get pdf text
+                    raw_text = get_pdf_text(pdf_docs)
+                    #get the text chunks
+                    text_chunks = get_text_chunks(raw_text)
+                    # st.write(raw_text)  # 显示处理后的文本
+                    st.write(text_chunks)
+                    #create vector store
+                    vectorstore = get_vectorstore(text_chunks)
+                    #create conversation chain
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+            else:
+                st.warning("Please upload at least one PDF file.")  # 如果没有上传文件则显示警告
 
 if __name__ == '__main__':
     main()
